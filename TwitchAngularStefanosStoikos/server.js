a665befabc2ae1efd22e0afcd06da1f6e60983d7
Stefanosss <stefanos.stoikos@student.ehb.be>
2021-05-16 22:40:12 +0200

    some changes again for heroku

--- TwitchAngularStefanosStoikos/server.js
+++ TwitchAngularStefanosStoikos/server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/twitch-angular-stefanos-stoikos"));
+app.use(express.static("./dist/TwitchAngularStefanosStoikos"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/twitch-angular-stefanos-stoikos/" })
+  res.sendFile("index.html", { root: "dist/TwitchAngularStefanosStoikos/" })
 );
 
 // Start the app by listening on the default Heroku port
